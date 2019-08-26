4e4f08d6a12cc7503c0ff5a43c6025e15311f9fd
moonti <tmmoond8@gmail.com>
2019-08-26 23:19:42 +0900

    #29 SEO 작업 - react-helmet 으로  SEO 처리

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,23 +4,35 @@ import { ServerStyleSheet } from '../styles/typed-components';
 
 interface IProps {
   styleTags: Array<React.ReactElement<{}>>;
+  helmetContext: any;
 }
 
 export default class MyDocument extends Document<IProps> {
   static getInitialProps({ renderPage }) {
+    let app;
     const sheet = new ServerStyleSheet();
     const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />),
+      app = sheet.collectStyles(<App {...props} />),
+      app
     );
 
     const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
+    return { ...page, styleTags, helmetContext: app.helmetContext };
   }
 
   render() {
+    const { helmetContext } = this.props;
     return (
       <html>
         <Head>
+          {!!helmetContext && !! helmetContext.helmet && (
+            <>
+              {helmetContext.helmet.meta.toComponent()}
+              {helmetContext.helmet.link.toComponent()}
+              {helmetContext.helmet.title.toComponent()}
+              {helmetContext.helmet.script.toComponent()}
+            </>
+          )}
           {this.props.styleTags}
         </Head>
         <body>
