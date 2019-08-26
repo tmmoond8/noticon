155e2cc2fb5419a2ca83336a1b645fbc93519253
moonti <tmmoond8@gmail.com>
2019-08-26 23:57:11 +0900

    #37 release

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const PORT = process.env.REACT_APP_PORT || 3000;
 
 app
   .prepare()
@@ -14,9 +15,9 @@ app
       return handle(req, res)
     })
 
-    server.listen(3000, err => {
+    server.listen(PORT, err => {
       if (err) throw err
-      console.log('> Ready on http://localhost:3000')
+      console.log(`> Ready on http://localhost:${PORT}`)
     })
   })
   .catch(ex => {
