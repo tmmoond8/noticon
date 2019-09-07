8c7df6bc475f5667b191f4883fe0e2ecc263790a
moonti <tmmoond8@gmail.com>
2019-09-07 15:15:52 +0900

    con-57 use only english - message change ko to en - set env sheet name

--- lib/dataRequest.ts
+++ lib/dataRequest.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { Noticon } from '../types';
+const {
+  REACT_APP_SHEET_NAME: sheetName
+} = process.env;
 
 export const get = async (sheet: 'logo' | 'normal') => {
-  const result = await axios.get(`${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${sheet}`);
+  const result = await axios.get(`${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${sheetName || 'noticon'}`);
   const data = result.data.data;
   return data;
 }
 
 export const append = async (sheet: 'logo' | 'normal', noticon: Noticon) => {
-  // await axios.post(`${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${sheet}`, 
-  await axios.get(`${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${sheet}`, { params: noticon });
+  await axios.get(`${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${sheetName || 'noticon'}`, { params: noticon });
 }
