7049860b925e37a3ff0e757e8976a71d04f0d572
TaeminMoon <tmmoond8@gmail.com>
2020-10-18 06:54:21 +0900

    feat: add ga

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import styled from '@emotion/styled';
+import React from 'react';
 import { Layout, Content } from 'notion-ui';
+import ReactGA from 'react-ga';
 import Gallery from '../components/Gallery';
 import Aside from '../components/Aside';
 import Header from '../components/Header';
 
 export default function Index() {
+  React.useEffect(() => {
+      ReactGA.initialize('UA-141390268-2');
+      ReactGA.pageview(window.location.pathname);
+  }, []);
   return (
     <Layout.App
       leftMenus={Header.LeftMenus}
