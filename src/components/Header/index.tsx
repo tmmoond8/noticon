eb62f29caf30826623dea23dd90d8f6ad74578c1
tamm <tmmoond8@gmail.com>
2021-06-18 08:03:13 +0900

    feat: hits 달기

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,20 +7,6 @@ import Icons from '../Icon';
 import { Content, Icon, colors } from 'notion-ui';
 import { observer, useStore } from '../../stores';
 
-const NoticonLogo = styled(Icons.Noticon)`
-  margin: 0 4px 0 8px;
-`;
-const NoticonText = styled(Content.Text)`
-  margin: 4px 2px 0 4px;
-`;
-
-const LeftMenus = (
-  <>
-    <NoticonLogo />
-    <NoticonText as="P">Noticon</NoticonText>
-  </>
-);
-
 const SearchBar = observer(function () {
   const { icon } = useStore();
   const [search, setSearch] = useState('');
@@ -61,6 +47,30 @@ const StyledSearchBar = styled.label`
     font-size: 18px;
   }
 `;
+
+
+const NoticonLogo = styled(Icons.Noticon)`
+  margin: 0 4px 0 8px;
+`;
+const NoticonText = styled(Content.Text)`
+  margin: 4px 2px 0 4px;
+`;
+
+const Hits = styled.a`
+  display: flex;
+  align-items: center;
+  margin: 4px 0 0 24px;
+`;
+
+const LeftMenus = (
+  <>
+    <NoticonLogo />
+    <NoticonText as="P">Noticon</NoticonText>
+    <Hits href="https://hits.seeyoufarm.com">
+      <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fnoticon.tammolo.com&count_bg=%23373C3F&title_bg=%23373C3F&icon=&icon_color=%239E5454&title=hits&edge_flat=false" />
+    </Hits>
+  </>
+);
 
 const RightMenus = (
   <>
