ab50ebfb32c5ba27010d04834639c3a394207d79
tamm <tmmoond8@gmail.com>
2025-06-16 00:27:17 +0900

    chore:  네트리파이 함수 추가

--- netlify/functions/get-sheet.ts
+++ netlify/functions/get-sheet.ts
@@ -1,6 +1,21 @@
+import axios from 'axios';
+
 export const handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Netlify!' }),
-  };
+  try {
+    const response = await axios.get(
+      `${process.env.GOOGLE_SHEET_URL}?sheetName=${process.env.SHEET_NAME}`,
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: 'Failed to fetch data from Google Sheets',
+      }),
+    };
+  }
 };
