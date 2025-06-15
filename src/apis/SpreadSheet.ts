b4d4a6031eb321f1945219f620d7a6bdb36cddec
tamm <tmmoond8@gmail.com>
2025-06-15 23:48:16 +0900

    fix: 스프레드 시트 에러 수정

--- src/apis/SpreadSheet.ts
+++ src/apis/SpreadSheet.ts
@@ -5,6 +5,12 @@ export default {
   get: async (): Promise<AxiosResponse<{ data: Noticon[] }>> =>
     await axios.get(
       `${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${process.env.REACT_APP_SHEET_NAME}`,
+      {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        withCredentials: false,
+      },
     ),
   append: async (noticon: {
     id: string;
@@ -14,6 +20,12 @@ export default {
   }) =>
     await axios.get(
       `${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${process.env.REACT_APP_SHEET_NAME}`,
-      { params: noticon },
+      {
+        params: noticon,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        withCredentials: false,
+      },
     ),
 };
