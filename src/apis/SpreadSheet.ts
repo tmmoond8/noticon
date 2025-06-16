d7ba4f1257038172afea290c038d238ff074be63
tamm <tmmoond8@gmail.com>
2025-06-16 22:03:57 +0900

    feat: sheet api를 next api에서 호출

--- src/apis/SpreadSheet.ts
+++ src/apis/SpreadSheet.ts
@@ -3,9 +3,7 @@ import { Noticon } from '../types';
 
 export default {
   get: async (): Promise<AxiosResponse<{ data: Noticon[] }>> =>
-    await axios.get(
-      `${process.env.REACT_APP_GOOGLE_SHEET_URL}?sheetName=${process.env.REACT_APP_SHEET_NAME}`,
-    ),
+    await axios.get(`${process.env.REACT_APP_URL}/api/get-sheet`),
   append: async (noticon: {
     id: string;
     imgUrl: string;
